8a537dcaa77d93132fdb48637251ac9097a3e578
Edvard Eros <edvard.eros@yahoo.com>
2020-05-25 15:06:37 +0300

    Project skeleton created

---
+++ src/main/java/com/ortho/cephalometry/CephalometryApplication.java
@@ -0,0 +1,13 @@
+package com.ortho.cephalometry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CephalometryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CephalometryApplication.class, args);
+	}
+
+}
